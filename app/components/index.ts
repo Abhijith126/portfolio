65ff34146e510c0bdd65f96f3aec128727b5c710
Abhijith P <akshay.abhijith@gmail.com>
2025-02-26 18:33:56 +0100

    Remove unused code

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import CursorHighlight from "./CursorHighlight";
-import ProjectCard from "./ProjectCard";
 import Socials from "./Socials";
 
-export { Navbar, CursorHighlight, ProjectCard, Socials };
+export { Navbar, CursorHighlight, Socials };
